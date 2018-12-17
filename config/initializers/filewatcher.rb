4c0140ef872ed4a6de868a3f0b091b9b4bcb4f22
Luciano Costa <lcosta_96@hotmail.com.br>
2018-12-17 02:44:08 -0300

    CSV data being added to DB

--- config/initializers/filewatcher.rb
+++ config/initializers/filewatcher.rb
@@ -1,12 +1,19 @@
 Rails.application.config.after_initialize do
   require 'filewatcher'
+  require 'csv'
 
   Thread.new do
     Filewatcher.new('csv_points/*.csv').watch do |filename, event|
       if(event != :deleted)
         puts "File updated: " + filename
+
+        CSV.foreach(filename, :headers => true, :col_sep => ';') do |row|
+          Score.create(date: row['date'], username: row['username'],
+            uid: row['uid'], points: row['points']
+          )
+        end
+
       end
     end
   end
-
 end
